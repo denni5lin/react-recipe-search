933e8c8bc94dabb3159bc78a0c325b1fdd45c8e7
Dennis Lin <lin.dennis18@gmail.com>
2019-02-20 20:18:37 -0800

    Complete recipe component and home link

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
 const API_KEY = "834355ec87c5822fad1900f9523ba34a";
 
 class Recipe extends React.Component {
@@ -14,11 +16,37 @@ class Recipe extends React.Component {
     );
 
     const res = await req.json();
-    console.log(res.recipes[0]);
+    this.setState({ activeRecipe: res.recipes[0] });
+    console.log(this.state.activeRecipe);
   };
   render() {
-    console.log(this.props);
-    return <div>Recipe Component!</div>;
+    const recipe = this.state.activeRecipe;
+    return (
+      <div className="container">
+        {this.state.activeRecipe.length !== 0 && (
+          <div className="active-recipe">
+            <img
+              className="active-recipe__img"
+              src={recipe.image_url}
+              alt={recipe.title}
+            />
+            <h3 className="active-recipe__title">{recipe.title}</h3>
+            <h4 className="active-recipe__publisher">
+              Publisher: <span>{recipe.publisher}</span>
+            </h4>
+            <p className="active-recipe__website">
+              Website:
+              <span>
+                <a href={recipe.publisher_url}>{recipe.publisher_url}</a>
+              </span>
+            </p>
+            <button className="active-recipe__button">
+              <Link to="/">Go Home</Link>
+            </button>
+          </div>
+        )}
+      </div>
+    );
   }
 }
 
