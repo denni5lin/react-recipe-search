57fea35d4b864d015091e35a3d797b3bcefbb628
Dennis Lin <lin.dennis18@gmail.com>
2019-02-20 20:52:41 -0800

    Updates

---
+++ precache-manifest.d06d9dbac75646003417c2ecfb297c9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d24d9e8a0d170fdd1b51",
+    "url": "/react-recipe-search/static/js/runtime~main.d24d9e8a.js"
+  },
+  {
+    "revision": "4fe2a3321e203f720762",
+    "url": "/react-recipe-search/static/js/main.4fe2a332.chunk.js"
+  },
+  {
+    "revision": "04672882301af0bda245",
+    "url": "/react-recipe-search/static/js/2.04672882.chunk.js"
+  },
+  {
+    "revision": "4fe2a3321e203f720762",
+    "url": "/react-recipe-search/static/css/main.00b9317b.chunk.css"
+  },
+  {
+    "revision": "04672882301af0bda245",
+    "url": "/react-recipe-search/static/css/2.7ec132a4.chunk.css"
+  },
+  {
+    "revision": "ed741084c30526433e9b17cb06593ae5",
+    "url": "/react-recipe-search/index.html"
+  }
+];
